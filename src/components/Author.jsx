3c5c73aac3f054f5a08f0fbe3cf3253d88f1a995
Foster <72973015+Foster0123@users.noreply.github.com>
2023-01-19 01:12:31 +0530

    Initial Commit 3 Calculator

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Author = () => {
 	return(
 		<div className="author">
-			<header>Coded By Foster Z</header>
+			<header>Coded By <a href="https://fosterz.netlify.app/"><strong>Foster Z</strong></a></header>
 		</div>
 	)
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,22 +3,32 @@ import Button from 'react-bootstrap/Button';
 
 const Calculator = () => {
 	const text = "6213123 X 123123"
+	const size = ""
+	const color = ""
 	return (
 		<div className="calculator">
 			<div className="display-box">
-			{text}
+				{text}
 			</div>
 			<div className="button-box">
-				<Button id="one" type="button" variant="primary" size="lg">1</Button>
-				<Button id="two" type="button" variant="primary" size="lg">2</Button>
-				<Button id="three" type="button" variant="primary" size="lg">3</Button>
-				<Button id="four" type="button" variant="primary" size="lg">4</Button>
-				<Button id="five" type="button" variant="primary" size="lg">5</Button>
-				<Button id="six" type="button" variant="primary" size="lg">6</Button>
-				<Button id="seven" type="button" variant="primary" size="lg">7</Button>
-				<Button id="eight" type="button" variant="primary" size="lg">8</Button>
-				<Button id="nine" type="button" variant="primary" size="lg">9</Button>
-				<Button id="zero" type="button" variant="primary" size="lg">0</Button>
+				<Button id="one" type="button" variant={color} size={size}>1</Button>
+				<Button id="two" type="button" variant={color} size={size}>2</Button>
+				<Button id="three" type="button" variant={color} size={size}>3</Button>
+				<Button id="four" type="button" variant={color} size={size}>4</Button>
+				<Button id="five" type="button" variant={color} size={size}>5</Button>
+				<Button id="six" type="button" variant={color} size={size}>6</Button>
+				<Button id="seven" type="button" variant={color} size={size}>7</Button>
+				<Button id="eight" type="button" variant={color} size={size}>8</Button>
+				<Button id="nine" type="button" variant={color} size={size}>9</Button>
+				<Button id="zero" type="button" variant={color} size={size}>0</Button>
+				<Button id="decimal" type="button" variant={color} size={size}>.</Button>
+				<Button id="all-clear" type="button" variant={color} size={size}>AC</Button>
+				<Button id="clear" type="button" variant={color} size={size}>C</Button>
+				<Button id="multiply" type="button" variant={color} size={size}>X</Button>
+				<Button id="division" type="button" variant={color} size={size}>/</Button>
+				<Button id="addition" type="button" variant={color} size={size}>+</Button>
+				<Button id="subtraction" type="button" variant={color} size={size}>-</Button>
+				<Button id="equals" type="button" variant={color} size={size}>=</Button>
 			</div>
 		</div>
 	)
