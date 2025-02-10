8c2251759ee43f454ec10dac3e8b2d6d88c8e470
Foster Z <fosterunlimited0123@gmail.com>
2025-02-10 15:46:10 +0530

    feat: add new operators & rearrange button position

--- src/components/calculator/Calculator.component.jsx
+++ src/components/calculator/Calculator.component.jsx
@@ -16,10 +16,18 @@ const Calculator = () => {
 	const [operator, setOperator] = useState(false)
 
 	const handleInput = (e) => {
-		if(text.length < 21) {
+		if (text.length < 21) {
 			setText((text) => [...text, e.target.value])
 		}
-		
+		console.log(text[text.length])
+	}
+	const handleDecmialInput = (e) => {
+		if (text[text.length - 1] === ".") {
+			setText((text) => [...text, ""])
+		}
+		else {
+			setText((text) => [...text, e.target.value])
+		}
 	}
 	const clearDisplayInput = () => {
 		setText("")
@@ -28,18 +36,18 @@ const Calculator = () => {
 		setText("")
 		setResult("")
 	}
-	const removeLastInput =() => {
-		if(text.length !== 0) {
+	const removeLastInput = () => {
+		if (text.length !== 0) {
 			setText(text.slice(0, -1))
 		}
 	}
 	const evaluateInput = () => {
-		if(text.length > 0) {
+		if (text.length > 0) {
 			const input = text.join("")
 			setResult(math.evaluate(input))
 			console.log(input)
 		}
-		
+
 	}
 	return (
 		<div className="calculator__main">
@@ -56,24 +64,30 @@ const Calculator = () => {
 				</div>
 				<section className="calculator__buttons__combined">
 					<div className="calculator__buttons__number">
-						<Button size="lg" variant="secondary" value="1" onClick={handleInput}>1</Button>
-						<Button size="lg" variant="secondary" value="2" onClick={handleInput}>2</Button>
-						<Button size="lg" variant="secondary" value="3" onClick={handleInput}>3</Button>
-						<Button size="lg" variant="secondary" value="4" onClick={handleInput}>4</Button>
-						<Button size="lg" variant="secondary" value="5" onClick={handleInput}>5</Button>
-						<Button size="lg" variant="secondary" value="6" onClick={handleInput}>6</Button>
-						<Button size="lg" variant="secondary" value="7" onClick={handleInput}>7</Button>
-						<Button size="lg" variant="secondary" value="8" onClick={handleInput}>8</Button>
-						<Button size="lg" variant="secondary" value="9" onClick={handleInput}>9</Button>
-						<Button size="lg" variant="secondary" value="0" onClick={handleInput}>0</Button>
-						<Button size="lg" variant="danger" value="%" onClick={handleInput}>%</Button>
-						<Button size="lg" variant="danger" value="." onClick={handleInput}>.</Button>
+						<Button size="lg" variant="primary" value="1" onClick={handleInput}>1</Button>
+						<Button size="lg" variant="primary" value="2" onClick={handleInput}>2</Button>
+						<Button size="lg" variant="primary" value="3" onClick={handleInput}>3</Button>
+						<Button size="lg" variant="primary" value="4" onClick={handleInput}>4</Button>
+						<Button size="lg" variant="primary" value="5" onClick={handleInput}>5</Button>
+						<Button size="lg" variant="primary" value="6" onClick={handleInput}>6</Button>
+						<Button size="lg" variant="primary" value="7" onClick={handleInput}>7</Button>
+						<Button size="lg" variant="primary" value="8" onClick={handleInput}>8</Button>
+						<Button size="lg" variant="primary" value="9" onClick={handleInput}>9</Button>
+						<Button size="lg" variant="primary" value="0" onClick={handleInput}>0</Button>
+
+						<Button size="lg" variant="danger" value="^" onClick={handleInput}>x<sup style={{fontSize: 'small'}}>n</sup></Button>
+						<Button size="lg" variant="danger" value="sqrt(" onClick={handleInput}>&radic;</Button>
+						
+						<Button size="lg" variant="danger" value="" onClick={handleInput}>%</Button>
+						<Button size="lg" variant="danger" value="(" onClick={handleInput}>&#40;</Button>
+						<Button size="lg" variant="danger" value=")" onClick={handleInput}>&#41;</Button>
 					</div>
 					<div className="calculator__buttons__operator">
 						<Button size="lg" variant="danger" value="+" onClick={handleInput}>+</Button>
 						<Button size="lg" variant="danger" value="-" onClick={handleInput}>-</Button>
 						<Button size="lg" variant="danger" value="*" onClick={handleInput}>x</Button>
 						<Button size="lg" variant="danger" value="/" onClick={handleInput}>/</Button>
+						<Button size="lg" variant="danger" value="/" onClick={handleInput}>.</Button>
 					</div>
 				</section>
 
